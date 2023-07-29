62b07775e780e86219f41fc6bfb97d27f579417e
Sumit <thesumit0013@gmail.com>
2023-07-30 03:19:16 +0530

    Minor Changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="container">
+    <div className="container App">
       <div className="row">
         <div className="col">
           <TitleProfile />
